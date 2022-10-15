bcb5f4712d4a496e89a762cfb07d90f5fdfd21e2
MemoryLong <haifeng222222@sina.com>
2022-10-15 22:00:34 +0800

    1.自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+APP_NAME=scratch-gui
+
 cd /appdata/scratch
 
-git clone --recursive git@github.com:MemoryLong/scratch-gui.git
+PID=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'` && kill -9 ${PID} && echo "kill process "${PID}" finished!"
+
+# git clone --recursive https://github.com/MemoryLong/scratch-gui.git
+git clone https://github.com/MemoryLong/scratch-gui.git
 
 cd scratch-gui
-
+git pull  https://github.com/MemoryLong/scratch-gui.git
 npm install
 
 nohup npm start & sleep 1
